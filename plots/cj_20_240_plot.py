5755000e6397af06fed3525dbad6ad70c3fb6f93
drewleonard <drew.n.leonard.19@dartmouth.edu>
2017-12-08 15:46:38 +0000

    new runs and plots

---
+++ plots/cj_20_240_plot.py
@@ -0,0 +1,16 @@
+import pickle
+import matplotlib.pylab as plt
+
+cj_20_240 = pickle.load( open( "cj_20_240.p", "rb" ) )
+
+lists = sorted(cj_20_240.items())
+
+x, y = zip(*lists)
+
+fig = plt.figure()
+plt.plot(x, y)
+fig.suptitle('CJ-20s Attacking Andersen AFB, Baseline Parameters + 240 ALCMs', fontsize=12)
+plt.xlabel('Leaked CJ-20 ALCMs', fontsize=12)
+plt.ylabel('Aircraft Destroyed', fontsize=12)
+plt.grid()
+plt.show()
---
+++ plots/cj_20_baseline_plot.py
@@ -0,0 +1,16 @@
+import pickle
+import matplotlib.pylab as plt
+
+cj_20_baseline = pickle.load( open( "cj_20_baseline.p", "rb" ) )
+
+lists = sorted(cj_20_baseline.items())
+
+x, y = zip(*lists)
+
+fig = plt.figure()
+plt.plot(x, y)
+fig.suptitle('CJ-20s Attacking Andersen AFB, Baseline Parameters', fontsize=14)
+plt.xlabel('Leaked CJ-20 ALCMs', fontsize=14)
+plt.ylabel('Aircraft Destroyed', fontsize=14)
+plt.grid()
+plt.show()
